6a73d4c880f2ba70db6b4292db12b64c1edad2fa
“andrerleao”​ <andre.leao.xoom@gmail.com>
2020-02-09 03:49:46 -0200

    Parte 3 - configurações helper e plugin.

--- codecept.conf.js
+++ codecept.conf.js
@@ -9,22 +9,101 @@ exports.config = {
   output: './output',
   helpers: {
     WebDriver: {
-      url: 'http://localhost',
-      browser: 'chrome'
+      url: "https://medium.com/@andre.r.leao",
+      browser: "chrome",
+      logLevel: "error",
+      logOutput: ".//logs//output//webdriver",
+      screenshotOnReject: true,
+      smartWait: 5000,
+      windowSize: "1920x1080", // largura x altura.
+      timeouts: {
+        script: 10000,
+        "page load": 10000
+      },
+      desiredCapabilities: {
+        chromeOptions: {
+          args: ["incognito", "--lang=pt-BR"],
+          //args: ["incognito", "--headless", "--disable-gpu", "--lang=pt-BR"],
+          w3c: false
+        }
+      }
+    },
+    Mochawesome: {
+      uniqueScreenshotNames: "true"
     }
   },
   include: {
     I: './steps_file.js'
   },
   bootstrap: null,
-  mocha: {},
-  name: 'tests-with-codeceptjs',
+  mocha: {
+    reporterEnabled: "mochawesome",
+    reporterOptions: {
+      "codeceptjs-cli-reporter": {
+        stdout: "-",
+        options: {
+          verbose: true,
+          steps: true
+        }
+      },
+      mochawesome: {
+        stdout: "-",
+        options: {
+          reportDir: ".//logs//output",
+          reportFilename: "report",
+          reportPageTitle: "tests-with-codeceptjs - Acceptance Tests",
+          reportTitle: "tests-with-codeceptjs - Acceptance Tests",
+          autoOpen: true,
+          enableCode: true,
+          inlineAssets: true,
+          overwrite: true,
+          quiet: false
+        }
+      }
+    }
+  },  
   plugins: {
-    retryFailedStep: {
-      enabled: true
+    wdio: {
+      enabled: true,
+      services: ["selenium-standalone"],
+      seleniumArgs: {
+        version: "3.141.59",
+        baseURL: "https://selenium-release.storage.googleapis.com",
+        basePath: "./node_modules/selenium-standalone/.selenium",
+        logsFile: "./logs/output/seleniumDebug.log",
+        drivers: {
+          chrome: {
+            version: "80.0.3987.16",
+            arch: process.arch,
+            baseURL: "https://chromedriver.storage.googleapis.com"
+          }
+        }
+      },
+      seleniumInstallArgs: {
+        version: "3.141.59",
+        baseURL: "https://selenium-release.storage.googleapis.com",
+        drivers: {
+          chrome: {
+            version: "80.0.3987.16",
+            arch: process.arch,
+            baseURL: "https://chromedriver.storage.googleapis.com"
+          }
+        }
+      }
     },
     screenshotOnFail: {
       enabled: true
+    },
+    autoDelay: {
+      enabled: true,
+      delayBefore: 300,
+      delayAfter: 300
+    },
+    stepByStepReport: {
+      enabled: true,
+      screenshotsForAllureReport: true,
+      deleteSuccessful: true
     }
-  }
+  },
+  name: 'tests-with-codeceptjs'
 }
