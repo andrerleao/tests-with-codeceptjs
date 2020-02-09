19f4d13005d434ce25ad32ed97c2b5e8b6c1ccdd
“andrerleao”​ <andre.leao.xoom@gmail.com>
2020-02-09 00:18:32 -0200

    initial commit

---
+++ codecept.conf.js
@@ -0,0 +1,30 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// HEADLESS=true npx codecept run
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    WebDriver: {
+      url: 'http://localhost',
+      browser: 'chrome'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'tests-with-codeceptjs',
+  plugins: {
+    retryFailedStep: {
+      enabled: true
+    },
+    screenshotOnFail: {
+      enabled: true
+    }
+  }
+}
---
+++ test_hello_world_test.js
@@ -0,0 +1,9 @@
+'use strict'
+
+Feature('Feature hello');
+
+Scenario('test something', (I) => {
+
+    I.say('teste do cenário hello world');
+
+});
